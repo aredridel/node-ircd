5b5c37a496e1730198eef0fd5d3c45aa34694bcf
Aria Stewart <aredridel@nbtsc.org>
2010-11-08 20:41:11 -0700

    Pass MUC->IRC joins

--- bin/ircmuc.js
+++ bin/ircmuc.js
@@ -1,6 +1,12 @@
 var xmpp = require('node-xmpp')
 var MUC = require('xmpp-muc')
 var irc = require('irc')
+var util
+try {
+	util = require('util')
+} catch(e) {
+	util = require('sys')
+}
 
 var MY_JID = 'test.0x42.net'
 
@@ -12,9 +18,19 @@ var ircd = irc.createDaemon('test.0x42.net')
 
 ircd.listen('6668')
 
+var mucChannels = {}
+var mucUsers = {}
+var ircChannels = {}
+var ircUsers = {}
+
 muc.on('channel', function(channel) {
-	channel.on('join', function(user) {
-		console.log("MUC", channel, "got", user)
+	if(!ircChannels[channel.jid]) ircChannels[channel.jid] = ircd.getChannel('#'+channel.jid.user)
+	channel.on('join', function(user, nick) {
+		console.log(util.inspect(user))
+		if(!ircUsers[user]) ircUsers[user] = new irc.User(user.user, '', null, 'XMPP User', user.domain, ircNick(nick))
+		ircChannels[channel.jid].join(ircUsers[user])
+		ircUsers[user].join(ircChannels[channel.jid])
+		console.log("MUC", channel, "got", user, "using IRC channel", ircChannels[channel.jid])
 	})
 	channel.on('part', function(user, message) {
 		console.log("MUC", channel, "lost", user)
@@ -32,3 +48,7 @@ ircd.on('channel', function(channel) {
 })
 
 process.on("SIGPIPE", function() { })
+
+function ircNick(str) {
+	return str.replace(/[^a-zA-Z0-9\[\]\\\`\^\{\}]/, '_')
+}
