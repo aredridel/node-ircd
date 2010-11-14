7c8e54af23adc0d18e4c95344ce64b3241e3788a
Aria Stewart <aredridel@nbtsc.org>
2010-11-13 20:52:55 -0700

    In IRCMUC, pass some messages across!

--- bin/ircmuc.js
+++ bin/ircmuc.js
@@ -23,14 +23,36 @@ var mucUsers = {}
 var ircChannels = {}
 var ircUsers = {}
 
+function MUCIRCUser(username, mode, unused, realname, hostname, nick) {
+	irc.User.call(this, username, mode, unused, realname, hostname, nick)
+}
+
+util.inherits(MUCIRCUser, irc.User)
+
+MUCIRCUser.prototype.send = function send(message) {
+	this.emit('message', message)
+}
+
 muc.on('channel', function(channel) {
 	if(!ircChannels[channel.jid]) ircChannels[channel.jid] = ircd.getChannel('#'+channel.jid.user)
+	var ircc = ircChannels[channel.jid]
 	channel.on('join', function(user, nick) {
+		console.log("MUC", channel, "got", user, "as", nick, "using IRC channel", ircc)
 		console.log(util.inspect(user))
 		if(!ircUsers[user]) ircUsers[user] = new irc.User(user.user, '', null, 'XMPP User', user.domain, ircNick(nick))
-		ircChannels[channel.jid].join(ircUsers[user])
-		ircUsers[user].join(ircChannels[channel.jid])
-		console.log("MUC", channel, "got", user, "using IRC channel", ircChannels[channel.jid])
+		var ircu = ircUsers[user]
+		ircc.join(ircUsers[user])
+		ircu.join(ircChannels[channel.jid])
+		ircu.on('message', function(message) {
+			if(message.command != 'PRIVMSG') return
+			if(!message.params[1]) return
+			console.log("MUC got ", message, "FROM IRC!")
+			var s = new xmpp.Element('message', {
+				from: channel.jid + '/' + message.sender.nick, 
+				type: 'groupchat'})
+			s.c('body').t(message.params[1])
+			channel.send(s)
+		})
 	})
 	channel.on('part', function(user, message) {
 		console.log("MUC", channel, "lost", user)
@@ -39,8 +61,13 @@ muc.on('channel', function(channel) {
 
 ircd.on('channel', function(channel) {
 	console.log("IRC ", channel)
+	var mucc = muc.getChannel(channel.name.slice(1) +'@'+MY_JID)
 	channel.on('join', function(user) {
 		console.log("IRC", channel, "got", user)
+		var s = new xmpp.Element('presence', {
+			from: mucc.jid + '/' + user.nick})
+		s.c('x', {xmlns: 'http://jabber.org/protocol/muc#user'})
+		mucc.send(s)
 	})
 	channel.on('part', function(user, message) {
 		console.log("IRC", channel, "lost", user)
