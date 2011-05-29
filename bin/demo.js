b9442d69978281a3d20bedc61eb70228ce23bb83
Aria Stewart <aredridel@nbtsc.org>
2011-05-29 16:28:37 -0600

    Add demo IRCD with lots of features

---
+++ bin/demo.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var irc = require('ircd')
+
+var ircd = irc.createDaemon('test.0x42.net')
+
+process.on("SIGPIPE", function() { })
+
+ircd.listen('6667')
+
+ircd.on('channel', function(channel) {
+	console.log(channel)
+	channel.on('join', function(user) {
+		console.log("User " + user.nick + " joined " + channel.name)
+	})
+	channel.on('topic', function(topic, user) {
+		console.log("Topic on channel " + channel.name + " changed to " + topic + " by " + user.nick)
+	})
+})
