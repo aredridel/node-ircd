90e4e24df1cf1a4b7e3ae5cdf0128e393b4c63de
Aria Stewart <aredridel@nbtsc.org>
2011-07-09 09:06:14 -0400

    Move logging to the library-using code

--- bin/demo.js
+++ bin/demo.js
@@ -37,6 +37,28 @@ process.on("SIGPIPE", function() { })
 ircd.listen('6667')
 app.listen('6680')
 
+function attachDebug(o) {
+	o.on('log.debug', function(l) {
+		console.log(l)
+	})
+	o.on('log.notice', function(l) {
+		console.log(l)
+	})
+	o.on('error', function(e) {
+		console.log("ERROR: "+e.message+"\n\t"+e.stack.join("\n\t"))
+	})
+}
+
+attachDebug(ircd)
+
+ircd.on('client', function(c) {
+	console.log('New connection')
+	attachDebug(c)
+	c.on('pingTimeout', function() {
+		console.log('Disconnecting client for ping timeout')
+	})
+})
+
 ircd.on('channel', function(channel) {
 	channels[channel.name] = channel
 	channel.on('join', function(user) {
